114ad0181d453f59ca9c8cca4a7a3ff97c5fe24f
devel0w0per <s.dorofeev00@yandex.ru>
2020-07-02 20:39:40 +0300

    added max width

--- vuexy-html-admin/chunks/auth_card_wrapper.php
+++ vuexy-html-admin/chunks/auth_card_wrapper.php
@@ -3,7 +3,7 @@
         <div class="content-body">
             <div class="row flexbox-container">
                 <div class="col-12 d-flex justify-content-center">
-                    <div class="card bg-authentication rounded-0 mb-0">
+                    <div class="mw-40 card bg-authentication rounded-0 mb-0">
                         <div class="row m-0">
                             <div class="col-12 p-2">
                                 <div class="card rounded-0 mb-0 px-2">
